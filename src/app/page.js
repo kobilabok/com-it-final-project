7e901fcb350a2ff49a4fe91c781caadfdbbee765
kobilabok <kobilabok@gmail.com>
2024-09-19 18:38:37 -0600

    Home page was added

--- src/app/page.js
+++ src/app/page.js
@@ -1,94 +1,114 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import starImage from "./assets/images/star.png";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div class="container">
+      <header class="header">
+        <nav>
+          <div class="logo"><a href="./home.html"><h3>TalentHub</h3></a></div>
+          <ul>
+            <li><a href="./home.html">Home</a></li>
+            <li><a href="./about.html">About Us</a></li>
+            <li><a href="./shopping-cart.html">Cart</a></li>
+          </ul>
+          <button class="btn-login" onclick="window.open('./login.html', '_self');">Login</button>
+        </nav>
+      </header>
+      <div class={styles.content}>
+        <main>
+          <h1>Welcome to TalentHub</h1>
+          <p>
+            Check out our talented artist to bring the perfect touch of intimacy
+            to your event. From accordions to ukuleles, from bagpipes to violins,
+            you can find em all here! At TalentHub we have already compiled all
+            the information transparently for you. Do you like one or more service
+            providers? Request up to four comparison offers quickly, easily and
+            free of charge.
+          </p>
+          <div class="dropdown">
+            <select
+              class={styles.select}
+              name="artists"
+              id="artists"
+              onchange="window.location.href=this.value;"
+            >
+              <option disabled selected>Please select an artist</option>
+              <option value="./list-of-artists.html">Portrait Painter</option>
+              <option value="./list-of-painters.html">Painters</option>
+              <option value="./list-of-photographers.html">Photographer</option>
+              <option value="./list-of-artists.html">DJ's</option>
+              <option value="./list-of-artists.html">Web Developer</option>
+            </select>
+          </div>
+          <div class={styles.reviews}>
+            <h3>Reviews</h3>
+          </div>
+          <div class={styles.tiles}> 
+            <div class={styles.tile}>
+              <p>
+                <Image src={starImage} alt="reviews star" />
+                <Image src={starImage} alt="reviews star" />
+                <Image src={starImage} alt="reviews star" />
+                <Image src={starImage} alt="reviews star" /><br />
+                  Eileen contacted me right away and I was so happy with the budget and she is able to 
+                  paint my portrait!
+                <br /><br /><b>02 Jun 2024</b>
+              </p>
+            </div>
+            <div class={styles.tile}>
+              <p>
+                <Image src={starImage} alt="reviews star" />
+                <Image src={starImage} alt="reviews star" />
+                <Image src={starImage} alt="reviews star" />
+                <Image src={starImage} alt="reviews star" /><br />
+                Will Reeb is ABSOLUTELY the BEST! He is not only incredibly talented, his costume and show 
+                are top notch. He interacts with his audience and truly gives them the best entertainment. 
+                <br /><br /><b>28 May 2024</b>
+                </p>
+            </div>  
+            <div class={styles.tile}>
+              <p>
+                <Image src={starImage} alt="reviews star" />
+                <Image src={starImage} alt="reviews star" />
+                <Image src={starImage} alt="reviews star" />
+                <Image src={starImage} alt="reviews star" />
+                <Image src={starImage} alt="reviews star" /><br />
+                Very professional, punctual, knows his stuff, good listener. He is an amazing person and we at Newport Harbour cannot WAIT to have him back here! We would give him 10 stars if we could!
+              <br /><br /><b>26 Oct 2023</b>
+              </p>
+            </div>
+          </div>  
+        </main>
+      </div>
+      <footer>
+        <div class="footer">
+          <h2>Questions or Concerns? Call 1800-678-2233</h2>
+  
+          <div class="row">
+            <div class="col">
+              <a href="#">© 2024</a>
+              <a href="#">Talenthub.com Global Limited.</a>
+              <a href="#">Terms & Conditions</a>
+            </div>
+            <div class="col">
+              <a href="mailto: info@talentHub.com">info@talentHub.com</a>
+              <a href="#">Corporate Information</a>
+              <a href="#">Investor Relations</a>
+            </div>
+            <div class="col">
+              <a href="#">Help Center</a>
+              <a href="#">Legal Notice</a>
+              <a href="#">Privacy</a>
+            </div>
+            <div class="col">
+              <a href="#">FAQ</a>
+              <a href="#">Account</a>
+              <a href="#">Careers</a>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
       </footer>
     </div>
   );
