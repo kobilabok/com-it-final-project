5045173223486b113c7271265dc27544b4147ad8
kobilabok <kobilabok@gmail.com>
2024-09-30 19:20:25 -0600

    Fetched photographer and package data

---
+++ src/app/artist-offers-photographers-packages-data/page.js
@@ -0,0 +1,12 @@
+import db from '../db/firestore'
+import { getDocs, collection } from 'firebase/firestore'
+
+const packages = async () => {
+    const snapshot = await getDocs((collection(db, 'packages')));
+
+    const documents = snapshot.docs.map(doc => doc.data())
+    console.log(documents);
+    return <div>{documents.map(doc => <div key={doc.id}>Package Id {doc.id} | Description {doc.description} | Price {doc.price}</div>)}</div>;
+}
+
+export default packages;
